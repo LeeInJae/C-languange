6e3747bffeb9d84cd021753d77170bfa35d86e66
LeeInJae <cowspirit@naver.com>
2014-12-18 02:16:20 +0900

    부모클래스와 자식클래스의 사이즈비교문 추가
    상속 및 업캐스트에 대한 내용
    
    상속관련 연습 및 virtual 키워드를 활용한 다형성
    
    동적할당한 메모리를 해제해주고나서는 포인터 주소를 nullptr로 초기화해주는 것이 안정적임.
    
    auto_ptr 실습예제
    
    add TryCatch & polymorphic

--- DocWriter/DocWriter/main.cpp
+++ DocWriter/DocWriter/main.cpp
@@ -1,5 +1,7 @@
 #include "DocWriter.h"
 #include "HTMLWriter.h"
+using namespace std;
+#include <iostream>
 
 int main(void){
 	DocWriter * docW1 = new DocWriter("test.txt", "good programmer yeah!");
@@ -10,6 +12,14 @@ int main(void){
 	htmlW1->SetContent("I muse be a good programmer~!");
 	htmlW1->SetFont("Arial", 16, "blue");
 	htmlW1->Write();
+	
+	cout << sizeof(DocWriter) << endl;
+	cout << sizeof(HTMLWriter) << endl;
+
+	HTMLWriter * pHtml = new HTMLWriter();
+	DocWriter * pDoc = pHtml;
+	pDoc->SetContent("hihihihihi");
+	pDoc->Write();
 	delete docW1;
 	delete htmlW1;
 	getchar();
